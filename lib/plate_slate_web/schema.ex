9818b60f5eeb986a71bd56fd3f6f829001e8965b
jswob <jacswob@gmail.com>
2022-10-27 16:52:00 +0200

    absinthe base

---
+++ lib/plate_slate_web/schema.ex
@@ -0,0 +1,13 @@
+defmodule PlateSlateWeb.Schema do
+	use Absinthe.Schema
+
+	query do
+		field :menu_items, list_of(:menu_items)
+	end
+
+	object :menu_item do
+		field :id, :id
+		field :name, :string
+		field :description, :string
+	end
+end
