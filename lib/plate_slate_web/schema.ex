f76a725cae528ced5ab80a30f69f6641ad5c73ac
jswob <jacswob@gmail.com>
2022-10-28 09:34:33 +0200

    add more descriptive documentation to menu items schema

--- lib/plate_slate_web/schema.ex
+++ lib/plate_slate_web/schema.ex
@@ -3,16 +3,22 @@ defmodule PlateSlateWeb.Schema do
 	alias PlateSlate.{Menu, Repo}
 
 	query do
-		@desc "The list of available items on the menu"
+		@desc "The list of available items on the menu."
 		field :menu_items, list_of(:menu_items) do
 			resolve fn _, _, _ ->
 				{:ok, Repo.all(Menu.Item)} end
 		end
 	end
 
+	@desc "The item on a menu."
 	object :menu_items do
+		@desc "The unique identifier of item."
 		field :id, :id
+
+		@desc "The name (label) of the item."
 		field :name, :string
+
+		@desc "The description (ingredients, weight, etc.) of item."
 		field :description, :string
 	end
 end
