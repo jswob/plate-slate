e87beee488806a8e968c802fad83dcf842f58d88
jswob <jacswob@gmail.com>
2022-10-28 08:18:17 +0200

    chore: add missing packages

--- lib/plate_slate_web/schema.ex
+++ lib/plate_slate_web/schema.ex
@@ -1,11 +1,15 @@
 defmodule PlateSlateWeb.Schema do
 	use Absinthe.Schema
+	alias PlateSlate.{Menu, Repo}
 
 	query do
-		# field :menu_items, list_of(:menu_items)
+		field :menu_items, list_of(:menu_items) do
+			resolve fn _, _, _ ->
+				{:ok, Repo.all(Menu.Item)} end
+		end
 	end
 
-	object :menu_item do
+	object :menu_items do
 		field :id, :id
 		field :name, :string
 		field :description, :string
